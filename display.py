ec4c27d10e94b3272a876282ea9afbc54be91ccc
Matt Goodhue <goodhue.m@northeastern.edu>
2023-12-05 01:13:15 -0500

    Finished Game Logic and Display

--- display.py
+++ display.py
@@ -1,8 +1,99 @@
 import tkinter as tk
-import random as rand
+from tkinter import font as tkFont
 import gamelogic as gl
 
-class GameBoard:
 
-    def __init__(self):
+class GameBoard(tk.Frame):
+
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.master.title('2048 Game')
+        self.master.bind("<Key>", self.key_event)
+        self.win_label = tk.Label(self, text='', font=('Calibri', 20, 'bold'), fg='green')
+        self.win_label.grid(row=gl.Board.SIZE, column=0, columnspan=gl.Board.SIZE, sticky="ew")
+        self.board = gl.Board()
+        self.draw_grid()
+
+    def draw_grid(self):
+
+        for i in range(gl.Board.SIZE):
+            self.columnconfigure(i, weight=1, minsize=50)
+            self.rowconfigure(i, weight=1, minsize=50)
+            for j in range(gl.Board.SIZE):
+                frame = tk.Frame(
+                    master=self,
+                    relief=tk.RAISED,
+                    borderwidth=10
+                )
+                label_font_size = int((1/gl.Board.SIZE) * 100)
+                label_font = tkFont.Font(family='Calibri', size=label_font_size, weight='bold', )
+                label = tk.Label(master=frame, bg="maroon", fg="white", width=10, height=4, font=label_font, text=self.board.board[i][j])
+                frame.grid(row=i, column=j, padx=2, pady=2)
+                label.pack() 
+
+
+        scoreboard = tk.Frame(
+            master=self,
+            relief=tk.SUNKEN,
+            borderwidth=3
+        )
+        scoreboard.grid(row=gl.Board.SIZE, column=0, columnspan=gl.Board.SIZE, sticky="ew")
+
+        score_label = tk.Label(
+            master=scoreboard,
+            text="Score: 0",
+            font=('Calibri', 20, 'bold')
+        )
+        score_label.pack(fill=tk.BOTH, expand=True)
+
+                   
+
+    def update_grid(self):
+        for i in range(gl.Board.SIZE):
+            for j in range(gl.Board.SIZE):
+                label = self.grid_slaves(row=i, column=j)[0].winfo_children()[0]
+                label.config(text=self.board.board[i][j])
+
+        score_label = self.grid_slaves(row=gl.Board.SIZE, column=0)[0].winfo_children()[0]
+        score_label.config(text=f"Score: {self.board.score}")
+
+        if self.board.has_lost():
+            label.config(text="You Lose!")
+            self.master.destroy()
+
+        if self.board.has_won():
+            label.config(text="You Win!")
+            self.master.destroy()
+                
+
+    def key_event(self, event):
+        direction = ""
+        if event.keysym == "Up":
+            direction = "up"
+        elif event.keysym == "Down":
+            direction = "down"
+        elif event.keysym == "Left":
+            direction = "left"
+        elif event.keysym == "Right":
+            direction = "right"
+        # Method to test win screen
+        elif event.keysym == "m":
+            self.board.insta_win()
+            self.update_grid()
+        # Method to test lose screen
+        elif event.keysym == "l":
+            self.board.insta_lose()
+            self.update_grid()
+
+
+        if direction:
+            self.board.move(direction)
+            self.update_grid()
+
+window = tk.Tk()
+game_board = GameBoard(master=window)
+game_board.pack(fill=tk.BOTH, expand=True)
+window.mainloop()
+
+
         
--- gamelogic.py
+++ gamelogic.py
@@ -1,60 +1,109 @@
 import numpy as np
+import random
 
 class Board:
     SIZE = 4
 
-    def __init__(self):
-        self.board = np.zeros((4, 4))
+    def __init__(self, score=0):
+        self.board = self.make_board()
+        self.score = score
 
-    def has_won(self):
-        """
-        Returns true if a 2048 value is present.
-        Returns false otherwise.
-        """
-        return np.all(self.board == 2048)
-    
-    def has_lost(self):
-        """
-        Returns true if the board is full of values
-        that are not equal to 0 or 2048 and the player
-        has lost. 
-        Returns false otherwise.
-        """
-        return np.all((self.board != 0) and (self.board != 2048))
+    def make_board(self):
+        board = np.zeros((4, 4), dtype=int)
+        Board.add_val(board, 2)
+        Board.add_val(board, 2)
+        return board
     
     @staticmethod
-    def shift_zeroes(arr, to_front=True):
+    def add_val(arr, val):
+        zero_indices = np.argwhere(arr == 0)
+        random_index = np.random.choice(zero_indices.shape[0])
+
+        row_index, col_index = zero_indices[random_index]
+
+        arr[row_index, col_index] = val
+    
+    @staticmethod
+    def shift_zeroes(arr, to_front):
         """
         Shifts all non-zero elements of a 1-D
         array to the front or the back of the
-        array, depending on the boolean input to_front.
+        array, depending on the boolean argument to_front.
         """
-        non_zero_indices = np.nonzero(arr)
-        if to_front:
-            arr[:len(non_zero_indices)] = arr[non_zero_indices]
-            arr[len(non_zero_indices):] = 0
+        # Creates list of the indices of non-zero values within
+        # a single row/column
+        non_zero_indices = np.nonzero(arr)[0]
+        if len(non_zero_indices) > 0:
+            if to_front:
+                # The number of non-zero values in the list is taken and
+                # used to slice the array up to this number. The indices
+                # up to this number are then changed to the non-zero values,
+                # retaining their order. 
+                arr[:len(non_zero_indices)] = arr[non_zero_indices]
+                arr[len(non_zero_indices):] = 0
+            else:
+                arr[-len(non_zero_indices):] = arr[non_zero_indices]
+                arr[:-len(non_zero_indices)] = 0
+                
         else:
-            arr[len(non_zero_indices):] = arr[non_zero_indices]
-            arr[:len(non_zero_indices)] = 0
+            arr[:] = 0         
+
+    def combine(self, arr, to_front=None):
+        if to_front:
+            for i in range(len(arr) - 1):
+                if arr[i] == arr[i+1] and arr[i] != 0:
+                    arr[i] *= 2
+                    arr[i+1] = 0 
+                    self.score += arr[i]
+
+        else:
+            for i in range(len(arr) - 1):
+                if arr[i] == arr[i+1] and arr[i] != 0:
+                    arr[i] *= 2
+                    arr[i-1] = 0 
+                    self.score += arr[i]
+
+    def insta_win(self):
+        self.board[0, 0] = 2048
+
+    def insta_lose(self):
+        self.board[:] = 1
     
     def move(self, direction):
-        for i in range(Board.SIZE):
-            if direction == 'up':
-                self.shift_zeroes(self.board[i, :], to_front=True)
-            elif direction == 'down':
-                self.shift_zeroes(self.board[i, :], to_front=False)
-            elif direction == 'left':
-                self.shift_zeroes(self.board[:, i], to_front=True)
-            elif direction == 'right':
-                self.shift_zeroes(self.board[:, i], to_front=False)
-
-class Game:
-    
-    def __init__(self):
-        self.board = Board()
-        self.score = 0
-
-    
         
+        prev_board = np.copy(self.board)
+        for i in range(Board.SIZE):
 
+            if direction == 'left':
+                to_front = True
+                self.shift_zeroes(self.board[i, :], to_front)
+                self.combine(self.board[i, :], to_front)
+                self.shift_zeroes(self.board[i, :], to_front)
 
+            elif direction == 'right':
+                to_front = False
+                self.shift_zeroes(self.board[i, :], to_front)
+                self.combine(self.board[i, :], to_front)
+                self.shift_zeroes(self.board[i, :], to_front)
+                
+            elif direction == 'up':
+                to_front = True
+                self.shift_zeroes(self.board[:, i], to_front)
+                self.combine(self.board[:, i], to_front)
+                self.shift_zeroes(self.board[:, i], to_front)
+                
+            elif direction == 'down':
+                to_front = False
+                self.shift_zeroes(self.board[:, i], to_front)
+                self.combine(self.board[:, i], to_front)
+                self.shift_zeroes(self.board[:, i], to_front)
+        
+        if not np.array_equal(prev_board, self.board):
+            new_num = random.choice([2, 4])
+            self.add_val(self.board, new_num)
+
+    def has_won(self):
+        return np.any(self.board == 2048)
+
+    def has_lost(self):
+        return not np.any(self.board == 2048) and not np.any(self.board == 0)
